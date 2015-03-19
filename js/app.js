2b12a80ac3abb282c5ba6f7ba19725518dd2db1d
Alexey <alexeybondarenko@me.com>
2015-03-19 23:41:31 +0200

    Losted app.js in prev commit

---
+++ js/app.js
@@ -0,0 +1,34 @@
+'use strict';
+
+var App = angular.module('app', ['ui.router']);
+
+App.config(function ($stateProvider, $urlRouterProvider) {
+
+    $stateProvider
+        .state('login', {
+            url: '/login',
+            templateUrl: 'views/login.html'
+        })
+        .state('signin', {
+            url: '/signin',
+            templateUrl: 'views/signin.html'
+        })
+        .state('money-transfer', {
+            url: '/money-transfer',
+            templateUrl: 'views/money-transfer.html'
+        })
+        .state('permissions-request', {
+            url: '/permissions-request',
+            templateUrl: 'views/permissions-request.html'
+        })
+        .state('receive-transfer', {
+            url: '/receive-transfer',
+            templateUrl: 'views/receive-transfer.html'
+        })
+        .state('success-transfer', {
+            url: '/success-transfer',
+            templateUrl: 'views/success-transfer.html'
+        });
+
+    $urlRouterProvider.otherwise('/login');
+});
